ec28376761471f9b6f25616f921bd15f4a1217c4
nimshav <88726093+nimshav@users.noreply.github.com>
2024-12-22 00:55:48 +0200

    ?

--- Express.js
+++ Express.js
@@ -4,8 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Default route for unhandled requests
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
