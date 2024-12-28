6ad77a41f843299c63982d1b469cc5cbfcbc3e83
nimshav <88726093+nimshav@users.noreply.github.com>
2024-12-28 15:35:33 +0200

    fix4

--- Express.js
+++ Express.js
@@ -15,6 +15,5 @@ app.get('*', (req, res) => {
 
 // Start the server on the correct port for Heroku
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
